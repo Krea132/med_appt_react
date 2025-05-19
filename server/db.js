7162314fbf3d3b766728d4dcaf1cd854079c18a9
Krea132 <javierjavieronline@gmail.com>
2025-05-19 20:09:01 +0200

    [UPD] local mongodb configured

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://localhost:27017/med_appt";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
