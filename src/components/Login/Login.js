38d08e5c56fe1acb4fcf102c09ae8f763f258b40
Krea132 <javierjavieronline@gmail.com>
2025-05-23 10:35:25 +0200

    [NEW] converted login and sing up

---
+++ src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+    <div className="container-xl pt-5">
+        <div className="login-form bg-light mx-auto mb-5 p-3 text-center rounded" style={{"max-width": "510px"}}>
+          
+          <h2 className="fw-bold">Login</h2>
+
+          <p>
+            Are you a new member? <a href="../Sign_Up/Sign_Up.html" className="text-decoration-none" style={{color: "#E80320"}}> Sign Up Here</a>
+          </p>
+          
+          <form>
+            <div className="form-group text-start mb-3">
+              <label className="fw-bold mb-2" for="email">Email</label>
+              <input type="email" name="email" id="email" className="form-control p-3" placeholder="Enter your email" aria-describedby="helpId" />
+            </div>
+            
+            <div className="form-group text-start mb-3">
+              <label className="fw-bold mb-2" for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="form-control p-3"
+                placeholder="Enter your password"
+                aria-describedby="helpId"
+              />
+            </div>
+
+            <div className="d-grid gap-3 mb-3">
+              <button type="submit" className="btn btn-primary mr-1 p-2">Login</button> 
+              <button type="reset" className="btn btn-danger p-2">Reset</button>
+            </div>
+            <a href="../Sign_Up/Sign_Up.html" className="text-decoration-none" style={{color: "#E80320"}}>Forgot Password?</a>
+            
+          </form>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Sign_up/Sign_Up.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Sign_Ups.css";
+
+function Sign_Ups() {
+  return (
+    <div>
+    <div className="container-xl pt-5">
+        <div className="login-form bg-light mx-auto mb-5 p-3 text-center rounded" style={{"max-width": "510px"}}>
+          
+          <h2 className="fw-bold">Sign Up</h2>
+
+          <p>
+            Already a member? <a href="../Login/Login.html" className="text-decoration-none" style={{color: "#E80320"}}> Login</a>
+          </p>
+          
+          <form>
+            
+            <div className="form-group text-start mb-3">
+                <label className="fw-bold mb-2" for="role">Role</label>
+                <select type="text" name="role" id="role" required className="form-select p-3" aria-describedby="helpId">
+                    <option selected>Select your role</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>   
+                </select>
+            </div>
+            
+            <div className="form-group text-start mb-3">
+                <label className="fw-bold mb-2" for="name">Name</label>
+                <input type="text" name="name" id="name" required className="form-control p-3" placeholder="Enter your name" aria-describedby="helpId" />
+            </div>
+            
+            <div className="form-group text-start mb-3">
+                <label className="fw-bold mb-2" for="phone">Phone</label>
+                <input type="tel" name="phone" id="phone" required className="form-control p-3" placeholder="Enter your phone number" aria-describedby="helpId" />
+            </div>
+
+            <div className="form-group text-start mb-3">
+              <label className="fw-bold mb-2" for="email">Email</label>
+              <input pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" type="email" name="email" id="email" className="form-control p-3" placeholder="Enter your email" aria-describedby="helpId" />
+            </div>
+            
+            <div className="form-group text-start mb-3">
+              <label className="fw-bold mb-2" for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="form-control p-3"
+                placeholder="Enter your password"
+                aria-describedby="helpId"
+              />
+            </div>
+
+            <div className="d-grid gap-3 mb-3">
+              <button type="submit" className="btn btn-primary mr-1 p-2">Login</button> 
+              <button type="reset" className="btn btn-danger p-2">Reset</button>
+            </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sign_Ups;
+
