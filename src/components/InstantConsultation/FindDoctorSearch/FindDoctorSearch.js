fe8441d599801eae87ccc81d4c4c8efdcfb6c539
Krea132 <javierjavieronline@gmail.com>
2025-06-19 15:54:46 +0200

    [NEW] FindDoctorSearch

---
+++ src/components/InstantConsultation/FindDoctorSearch/FindDoctorSearch.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './FindDoctorSearch.css';
+import { useNavigate, Navigate } from 'react-router-dom';
+
+
+const initSpeciality = [
+    'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
+]
+
+const FindDoctorSearch = () => {
+    const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const [searchDoctor, setSearchDoctor] = useState('');
+    const [specialities, setSpecialities] = useState(initSpeciality);
+    const navigate = useNavigate();
+    const handleDoctorSelect = (speciality) => {
+        setSearchDoctor(speciality);
+        setDoctorResultHidden(true);
+        navigate(`/instant-consultation?speciality=${speciality}`);
+        window.location.reload();
+    }
+    return (
+        <div className='finddoctor bg-light text-start p-4' style={{'max-width':'480px'}}>
+            <h1 className='mt-0 mb-3 fw-bold h2' style={{'line-height':'1.3lh'}}>Find a doctor at your own ease</h1>
+            <img className='w-100 mb-3' src="/search.jpg" alt="Descripción de la imagen" />
+            <div className="home-search-container">
+                <div className="doctor-search-box">
+
+                    <div className='position-relative w-100'>
+                        <input type="text" className="form-control w-100 py-3 pe-5" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
+                        <svg style={{'top':'19px','right':'14px'}} className='bi bi-search position-absolute' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    </div>
+
+                    <div style={{'border-color':'#cccccc'}} className="search-doctor-input-results" hidden={doctorResultHidden}>
+                        {
+                            specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
+                                <span>
+                                    <svg className='bi bi-search' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                                    </svg>
+                                </span>
+                                <span>{speciality}</span>
+                                <span>SPECIALITY</span>
+                            </div>)
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FindDoctorSearch
