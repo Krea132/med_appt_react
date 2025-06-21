337a02fc44f8b9f17bcd308b921524dd27230131
Krea132 <javierjavieronline@gmail.com>
2025-06-21 22:10:49 +0200

    [NEW] Create the AppointmentForm component

---
+++ src/components/InstantConsultation/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
+    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [dateAppointment, setDateAppointment] = useState('');
+    const [selectedSlot, setSelectedSlot] = useState(null);
+  
+    const handleSlotSelection = (slot) => {
+      setSelectedSlot(slot);
+    };
+  
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      onSubmit({ name, phoneNumber });
+      setName('');
+      setPhoneNumber('');
+    };
+  
+    return (
+      <form onSubmit={handleFormSubmit} className="appointment-form bg-light mx-auto mb-5 p-3 rounded">
+        <div className="form-group mb-3">
+          <label  className="fw-bold mb-2" htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="form-control p-3"
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label  className="fw-bold mb-2" htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
+            className="form-control p-3"
+          />
+        </div>        
+        <div className="form-group mb-3">
+          <label  className="fw-bold mb-2" htmlFor="phoneNumber">Date of Appointment:</label>
+          <input
+            type="date"
+            id="phoneNumber"
+            value={dateAppointment}
+            onChange={(e) => setDateAppointment(e.target.value)}
+            required
+            className="form-control p-3"
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label className="fw-bold mb-2" htmlFor="timeSlot">Book Time Slot:</label>
+          <select id='timeSlot' class="form-select p-3"
+            required
+            value={selectedSlot}
+            onChange={(e) => handleSlotSelection(e.target.value)}>
+            <option selected>Select a time slot</option>
+            <option value="1">08:00</option>
+            <option value="2">09:45</option>
+            <option value="3">11:15</option>
+          </select>
+        </div>
+        <button className="btn btn-primary mr-1 p-2" type="submit">Book Now</button>
+      </form>
+    );
+  };
+
+export default AppointmentForm
--- src/components/InstantConsultation/DoctorCard/DoctorCard.js
+++ src/components/InstantConsultation/DoctorCard/DoctorCard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './DoctorCard.css';
-import AppointmentFormIC from '../AppointmentFormIC/AppointmentFormIC'
+import AppointmentForm from '../AppointmentForm/AppointmentForm'
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -55,7 +55,7 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
         <Popup
             style={{ backgroundColor: '#FFFFFF' }}
             trigger={
-              <button style={{ 'border-top-left-radius': '0', 'border-top-right-radius': '0'}} className={`btn btn-primary rounded-bottom mr-1 p-2 ${appointments.length > 0 ? 'cancel-appointment' : ''}`}>
+              <button style={{ 'border-top-left-radius': '0', 'border-top-right-radius': '0'}} className={`stretched-link btn w-100 btn-primary rounded-bottom mr-1 p-2 ${appointments.length > 0 ? 'btn-danger' : ''}`}>
                 {appointments.length > 0 ? (
                   <div>Cancel Appointment</div>
                 ) : (
@@ -69,32 +69,30 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
             onClose={() => setShowModal(false)}
           >
             {(close) => (
-              <div className="doctorbg" style={{ height: '100vh', overflow: 'scroll' }}>
-                <div>
-                  <div className="doctor-card-profile-image-container">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg>
-                  </div>
-                  <div className="doctor-card-details">
-                    <div className="doctor-card-detail-name">{name}</div>
-                    <div className="doctor-card-detail-speciality">{speciality}</div>
-                    <div className="doctor-card-detail-experience">{experience} years experience</div>
-                    <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
-                  </div>
+              <div className="doctorbg p-4">
+                <div className="text-center mb-5 pt-3">
+                  <img className='mx-auto display-block mb-3' style={{'max-height':'150px'}} src="/dr-image.jpg" alt="Descripción de la imagen" />
+                  <h4 className="fw-bold">{name}</h4>
+                  <div className="mb-1">{speciality}</div>
+                  <div style={{'color':'#888888'}} className="mb-2 fw-bold">{experience} years experience</div>
+                  <div className="fw-bold"><small>Ratings:</small> {ratings}</div>
                 </div>
 
                 {appointments.length > 0 ? (
                   <>
-                    <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
+                  <div className="text-center bg-light p-4 rounded">
+                    <h3>Appointment Booked!</h3>
                     {appointments.map((appointment) => (
                       <div className="bookedInfo" key={appointment.id}>
-                        <p>Name: {appointment.name}</p>
-                        <p>Phone Number: {appointment.phoneNumber}</p>
-                        <button onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
+                        <p>Name: <strong>{appointment.name}</strong></p>
+                        <p>Phone Number: <strong>{appointment.phoneNumber}</strong></p>
+                        <button className="btn btn-outline-danger p-2" onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
                       </div>
                     ))}
+                    </div>
                   </>
                 ) : (
-                  <AppointmentFormIC doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
+                  <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
                 )}
               </div>
             )}
