8a2c98ff04ac8eef6e8ee995d5f39154196c0c1f
Krea132 <javierjavieronline@gmail.com>
2025-07-20 18:58:59 +0200

    [WIP] preparing for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "add your theia server side url";
-console.log(
-    "API_URL :",
-    API_URL
-);
+export const API_URL = 
+window.location.hostname === "localhost" 
+    ? "http://localhost:8181" 
+    : "https://my-server-example.com/api";
+
+console.log("API_URL :",API_URL);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // instead of UglifyJsPlugin
+
+module.exports = {
+  mode: 'production', 
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'server/build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              esModule: false, 
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()], // compatible with Webpack 5
+  },
+};
